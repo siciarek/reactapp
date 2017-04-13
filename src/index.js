bc7d350fb4436bbd0b8fbcda5400aa2dde451f68
Jacek Siciarek <siciarek@gmail.com>
2017-04-13 11:28:43 +0200

    Działający kod

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routing from './app/Routing';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-ReactDOM.render((
-    <Routing />
-  ),
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
