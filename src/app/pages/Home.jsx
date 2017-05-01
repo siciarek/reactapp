33dfe73d39ce098b190d793cc8c962273616748f
Jacek Siciarek <siciarek@gmail.com>
2017-05-01 11:07:37 +0200

    Fix responsiveness

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -4,20 +4,20 @@ import config from '../config'
 
 import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 
-import ResponsiveReactGridLayout from 'react-grid-layout'
-const GridLayout = ResponsiveReactGridLayout.WidthProvider(ResponsiveReactGridLayout.Responsive)
+import {Responsive, WidthProvider} from 'react-grid-layout'
+const GridLayout = WidthProvider(Responsive)
 
 class Home extends React.Component {
 
   render() {
     return (
       <GridLayout
-        margin={[0,0]}
-        containerPadding={[0,0]}
         isDraggable={false}
         isResizable={false}
+        margin={[0, 0]}
+        containerPadding={[0, 0]}
       >
-        <Card key={1} data-grid={{w: 6, h: 1, x: 3, y: 0}}>
+        <Card key={1} data-grid={{x: 3, y: 0, w: 6, h: 2, static: false}}>
           <CardMedia overlay={<CardTitle title={config.appName} subtitle="Just sing a song"/>}>
             <img src={config.appPicture} alt="Mircorphone"/>
           </CardMedia>
