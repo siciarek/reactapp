fbc8f3474a03ae607835b49a106560a0cb3805c0
Jacek Siciarek <siciarek@gmail.com>
2017-07-02 20:40:24 +0200

    Modify stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import {storiesOf, action, linkTo} from '@kadira/storybook';
 
-import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-
-import {Provider} from 'react-redux'
-import store from '../app/store'
-
-
+import '../app/App.css'
 import '../app/components/AppHeader.css'
 import {AppHeader} from '../app/components'
 
 storiesOf('AppHeader', module)
-.add('With no params', () => <MuiThemeProvider muiTheme={getMuiTheme(baseTheme)}>
-    <AppHeader/>
-  </MuiThemeProvider>
-)
-.add('With title', () => <Provider store={store}>
-  <AppHeader title="Zażółć gęślą jaźń!"/>
-</Provider>
-)
+.add('With no params', () => <AppHeader/>)
+.add('With title', () => <AppHeader title="Zażółć gęślą jaźń!"/>)
 
