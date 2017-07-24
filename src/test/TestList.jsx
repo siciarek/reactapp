cc19a34127362791e714a43104ad783620201e86
Jacek Siciarek <siciarek@gmail.com>
2017-07-24 19:42:54 +0200

    Add table

--- src/test/TestList.jsx
+++ src/test/TestList.jsx
@@ -3,21 +3,66 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {fetchTestList} from './TestActions'
 
+import {Table, TableHead, TableBody, TableRow, TableCell, Checkbox} from 'material-ui'
+
 class TestList extends React.Component {
 
   componentWillMount() {
     this.props.init()
   }
 
+  onSelectAllClick = (event, id) => {
+    console.log(event, id)
+  }
+
+  handleClick = (event, id) => {
+    console.log(event, id)
+  }
+
+  handleKeyDown = (event, id) => {
+    console.log(event, id)
+  }
+
   render() {
     const {items} = this.props
 
-    return <div>
-      <h1>Test List</h1>
-      <pre>
-        {JSON.stringify(this.props.items, null, 4)}
-      </pre>
-    </div>
+    return <Table>
+      <TableHead>
+
+        <TableRow>
+          <TableCell checkbox>
+            <Checkbox onChange={this.onSelectAllClick}/>
+          </TableCell>
+          <TableCell numeric>#</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>First name</TableCell>
+          <TableCell>Last name</TableCell>
+          <TableCell>Info</TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        {
+          this.props.items.map((item, index) => {
+            return <TableRow hover
+                             key={item.id}
+                             tabIndex="-1"
+                             onClick={event => this.handleClick(event, item.id)}
+                             onKeyDown={event => this.handleKeyDown(event, item.id)}
+            >
+              <TableCell checkbox>
+                <Checkbox/>
+              </TableCell>
+              <TableCell numeric>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.firstName}</TableCell>
+              <TableCell>{item.lastName}</TableCell>
+              <TableCell>{item.info}</TableCell>
+            </TableRow>
+          })
+        }
+      </TableBody>
+    </Table>
   }
 }
 
