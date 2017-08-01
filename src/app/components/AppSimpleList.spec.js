8ba2174a869b17e4a173d62f4cd07967fa14ab5d
Jacek Siciarek <siciarek@gmail.com>
2017-08-02 00:52:06 +0200

    Update tests

---
+++ src/app/components/AppSimpleList.spec.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {renderComponent, renderShallowComponent, getProps} from '../../utils/testHelper'
+import {AppSimpleList} from '../components'
+import {lorem} from 'faker'
+
+describe('AppSimpleList', () => {
+
+  it('can render without error', () => {
+
+    expect(function () {
+      const component = renderComponent(<AppSimpleList/>)
+    }).not.toThrow()
+  })
+
+  it('has properly set default title', () => {
+
+    const title = AppSimpleList.defaultProps.title
+    const element = <AppSimpleList/>
+
+    const component = renderComponent(element)
+    const props = getProps(component)
+    expect(props.title).toBe(title)
+  })
+
+  it('has proper title when set', () => {
+
+    let i = 10;
+    let titles = ['Zażółć gęślą jaźń'];
+
+    while(i-- > 0) {
+      titles.push(lorem.sentence())
+    }
+
+    titles.map(title => {
+      const element = <AppSimpleList title={title}/>
+
+      const component = renderComponent(element)
+      const props = getProps(component)
+      expect(props.title).toBe(title)
+    })
+  })
+})
