c5effccbab5995cdb0e5303cfb5aad469e260617
Jacek Siciarek <siciarek@gmail.com>
2017-08-25 21:02:54 +0200

    Modify record view

--- src/record/components/RecordItem.jsx
+++ src/record/components/RecordItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {AppHeader, AppSpinner, AppFloatingActionButton} from '../../app/widgets'
+import {uniq} from 'lodash'
 
 class RecordItem extends React.Component {
 
@@ -18,6 +19,28 @@ class RecordItem extends React.Component {
       return null
     }
 
+
+    const sides = uniq(tracks.map(e => e.side))
+
+    let side = 0
+    let sideRow = ''
+
+    let tracksPerSide = tracks.map(e => {
+      sideRow = ''
+
+      if(sides.length > 1 && e.side !== side) {
+        side = e.side
+        sideRow = <div><br/><strong>Side {e.side}</strong></div>
+      }
+
+      return <li key={e.id}>
+        {sideRow}
+        <span style={{display: 'inline-block', paddingRight: 16}}>{e.length}</span>
+        {e.title}
+      </li>
+    })
+
+
     return <div>
       <AppHeader title={title}/>
 
@@ -28,15 +51,8 @@ class RecordItem extends React.Component {
 
         <img src={cover} alt={title} style={{width: 300, marginTop: 16, marginBottom: 16}}/>
 
-        <ul style={{margin:0, padding:0, listStyleType: 'none'}}>
-          {
-            tracks.map(e => {
-              return <li key={e.id}>
-                <span style={{display: 'inline-block', paddingRight: 16}}>{e.length}</span>
-                {e.title}
-                </li>
-            })
-          }
+        <ul style={{margin: 0, padding: 0, listStyleType: 'none'}}>
+          {tracksPerSide}
         </ul>
 
 
@@ -58,7 +74,8 @@ RecordItem.propTypes = {
 }
 
 RecordItem.defaultProps = {
-  init: () => {},
+  init: () => {
+  },
   item: {},
 }
 
