3583522ef5ff3ba14ec432d065981b3bcd6a4266
Jacek Siciarek <siciarek@gmail.com>
2017-08-26 23:31:07 +0200

    Refactor form helpers

--- src/user/components/ProfileForm.jsx
+++ src/user/components/ProfileForm.jsx
@@ -1,50 +1,39 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
-import {renderTextField, SubmitButton} from '../../utils/formHelpers'
+import {renderTextField, renderSwitch, renderGenderField, SubmitButton} from '../../utils/formHelpers'
 import {required, email, minLength3, maxLength32} from '../../utils/formValidators'
-import {SelectGenderField} from '../../app/widgets'
-import {FormControlLabel, Switch} from 'material-ui'
 
 const ProfileForm = ({handleSubmit}) => {
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field name="profileVisibleToThePublic" label="Profile visible to the public"
-               component={({label, input}) => <FormControlLabel
-                 label={label}
-                 control={<Switch checked={input.value} onChange={(event, value) => input.onChange(value)}/>}
-               />}/>
-      </div>
-      <div>
-        <Field name="gender"
-               component={({input, meta: {touched, error, warning}, ...custom}) => <SelectGenderField
-                 onChange={(event, value) => input.onChange(value)} {...input} {...custom}
-               />}/>
-      </div>
-      <div>
-        <Field name="firstName" label="First name" validate={[required, minLength3, maxLength32]}
-               component={renderTextField}/>
-      </div>
-      <div>
-        <Field name="lastName" label="Last name" validate={[required, minLength3, maxLength32]}
-               component={renderTextField}/>
-      </div>
-      <div>
-        <Field name="email" label="E-mail" validate={[required, email]} component={renderTextField}/>
-      </div>
-      <div>
-        <Field name="description" label="Description" component={renderTextField}/>
-      </div>
-      <div>
-        <Field name="info" label="Info" rowsMax={8} multiline={true} component={renderTextField}/>
-      </div>
-      <br/>
-      <div>
-        <SubmitButton/>
-      </div>
-    </form>
-  )
+  return <form onSubmit={handleSubmit}>
+    <div>
+      <Field name="profileVisibleToThePublic" label="Profile visible to the public" component={renderSwitch}/>
+    </div>
+    <div>
+      <Field name="gender" label="Gender" component={renderGenderField}/>
+    </div>
+    <div>
+      <Field name="firstName" label="First name" validate={[required, minLength3, maxLength32]}
+             component={renderTextField}/>
+    </div>
+    <div>
+      <Field name="lastName" label="Last name" validate={[required, minLength3, maxLength32]}
+             component={renderTextField}/>
+    </div>
+    <div>
+      <Field name="email" label="E-mail" validate={[required, email]} component={renderTextField}/>
+    </div>
+    <div>
+      <Field name="description" label="Description" component={renderTextField}/>
+    </div>
+    <div>
+      <Field name="info" label="Info" rowsMax={8} multiline={true} component={renderTextField}/>
+    </div>
+    <br/>
+    <div>
+      <SubmitButton/>
+    </div>
+  </form>
 }
 
 export default reduxForm({form: 'profileForm'})(ProfileForm)
--- src/utils/formHelpers.jsx
+++ src/utils/formHelpers.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
-import {TextField, RadioGroup} from 'material-ui'
-import {SubmitButton, SelectGenderField} from '../app/widgets'
+import {TextField, RadioGroup, FormControlLabel, Switch} from 'material-ui'
+
+import {
+  SubmitButton,
+  SelectGenderField,
+} from '../app/widgets'
 
 // http://redux-form.com/7.0.3/examples/material-ui/
 
-const renderTextField = props => {
-  const {label, input, meta: {touched, error, warning}, ...custom} = props
+const renderSwitch = ({label, input}) => <FormControlLabel
+  label={label}
+  control={<Switch checked={input.value} onChange={(event, value) => input.onChange(value)}/>}
+/>
 
+const renderTextField = ({label, input, meta: {touched, error, warning}, ...custom}) => <TextField
+  style={{marginTop: 8}}
+  autoComplete="off"
+  helperText={touched && error || touched && warning}
+  error={touched && error && error.length > 0}
+  label={label}
+  {...input}
+  {...custom}
+/>
 
-  return <TextField
-    style={{marginTop: 8}}
-    autoComplete="off"
-    helperText={touched && error || touched && warning}
-    error={touched && error && error.length > 0}
-    label={label}
+const renderGenderField = ({input, meta: {touched, error, warning}, ...custom}) => <SelectGenderField
+  onChange={(event, value) => input.onChange(value)} {...input} {...custom}
+/>
 
-    {...input}
-    {...custom}
-  />
-}
-
-const renderSelectGenderField = props => {
-  const {label, input, meta: {touched, error, warning}, ...custom} = props
-
-  return <SelectGenderField {...input} {...custom}/>
-}
-
-export {renderTextField, renderSelectGenderField, SubmitButton}
+export {renderTextField, renderSwitch, renderGenderField, SubmitButton}
