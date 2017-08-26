9c8cd38faa185bcae9fb654bd03944ab9076c738
Jacek Siciarek <siciarek@gmail.com>
2017-08-26 09:42:22 +0200

    RWD on Home

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
 import config from '../config'
-import Card, {CardContent, CardMedia} from 'material-ui/Card';
-import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
 
-const Home = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+    padding: 16,
+  },
+  image: {
+    width: '100%'
+  }
+})
+
+const Home = (props) => {
 
   const {appName, appDescription, appPicture} = config
+  const {classes} = props;
 
-  return <Card>
-      <CardContent>
-        <Typography type="headline" component="h2">
-          {appName}
-        </Typography>
-        <Typography component="p">
-          {appDescription}
-        </Typography>
-      </CardContent>
-    </Card>
+  return  <div className={classes.root}>
+    <Grid container spacing={24}>
+      <Grid item xs={12} sm={6}>
+        <h1>{appName}</h1>
+        <p>{appDescription}</p>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <img className={classes.image} src={appPicture}/>
+      </Grid>
+    </Grid>
+  </div>
 }
 
-Home.defaultProps = {}
+Home.defaultProps = {
+  classes: PropTypes.object.isRequired,
+}
 
-export default Home
+export default withStyles(styles)(Home)
